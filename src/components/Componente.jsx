04d90197b0ba2f7b776dd876290a4ec3b31acd73
santiago ruiz <santiagoruiz9416@gmail.com>
2022-10-28 20:49:22 -0300

    first commit in my branch

--- src/components/Componente.jsx
+++ src/components/Componente.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import "../styles/componente.css";
 
 function Prueba (prop) {
-    return(
-        <h2>Prueba: {prop.msj}</h2>
-    )
+  return(
+    <>
+      <h2>Prueba: {prop.msj}</h2>
+      <p>Trabajando desde mi rama</p>
+    </>
+  )
 };
 
 export default Prueba;
