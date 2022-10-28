7ed309c0c7bbd0ef14bc540251a865e00cbd0fe4
santiago ruiz <santiagoruiz9416@gmail.com>
2022-10-28 20:32:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Prueba from "./components/Componente";
+
+
+function App() {
+
+  return (
+    <>
+      <h1>{2 + 3}</h1>
+      <Prueba msj="Esto es una prop casera" />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Componente.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/componente.css";
+
+function Prueba (prop) {
+    return(
+        <h2>Prueba: {prop.msj}</h2>
+    )
+};
+
+export default Prueba;
